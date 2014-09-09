ce915cc82e168fd39fabc119f1c9b8b2b875e623
Joemar S Matulac <joemar.matulac@totalsofttech.com.ph>
2014-09-10 00:40:21 +0800

    Added client server broadcast and listening hacking

---
+++ IS_238/socketshack/streamclient.h
@@ -0,0 +1,9 @@
+/*
+ *  streamclient.h
+ *  
+ *
+ *  Created by Latin on 9/10/14.
+ *  Copyright 2014 __MyCompanyName__. All rights reserved.
+ *
+ */
+
---
+++ IS_238/socketshack/streamsrv.h
@@ -0,0 +1,9 @@
+/*
+ *  streamsrv.h
+ *  
+ *
+ *  Created by Latin on 9/10/14.
+ *  Copyright 2014 __MyCompanyName__. All rights reserved.
+ *
+ */
+
