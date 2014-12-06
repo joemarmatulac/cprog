f7ad945b03fe34b2fa7f8d05130dc32955d51019
Joemar Matulac <joemar.matulac@totalsofttech.com.ph>
2014-12-06 22:33:50 +0800

    Include request to host server using HTTP GET and calculate speed per second.

--- IS_238/iTerminal/iTerminal.c
+++ IS_238/iTerminal/iTerminal.c
@@ -10,81 +10,139 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#define BUFF_SIZE 1024
+#define MSG_STR_SIZE 1000000
 /*
- char *host =        "google.com";
- char *message_fmt = "GET /search?q=up+open+university HTTP/1.0\n\n";
- 
- char *host =        "search.yahoo.com";
- char *message_fmt = "GET //search?p=up+open+university HTTP/1.0\n\n";
- 
- char *host =        "www.bing.com";
- char *message_fmt = "GET /search?q=upou HTTP/1.0\n\n";
- 
- char *host =        "en.wikipedia.org";
- char *message_fmt = "GET /wiki/University_of_the_Philippines_Open_University HTTP/1.0\n\n";
- 
- char *host =        "youtube.com";
- char *message_fmt = "GET /?search_query=upou HTTP/1.0\n\n";
- 
- char *host =        "facebook.com";
- char *message_fmt = "GET /search=UPOpenUniversity HTTP/1.0\n\n";
- 
- char *host =        "twitter.com";
- char *message_fmt = "GET /search?q=UP HTTP/1.0\n\n";
+ * Used google and yahoo as it is the most popular and most visited site.
+ * These two sites are also the one which i can GET a response successfully
+ *  to which I can used received bytes relative to its speed.
+ * (1) google.com
+ * (2) search.yahoo.com
  */
-
-void *receivedMessagethreadListener(int *arg);
-
 int main(int argc, char *argv[]){
-    time_t start_t, end_t;
-    double diff_t;
+    /*
+     * Constant Declation
+     */
+    static const char GOOGLE_HOST[] = "google.com";
+    static const char GOOGLE_QUERYSTR[] = "GET /search?q=up+open+university HTTP/1.0\n\n";
+    static const char YAHOO_HOST[] = "search.yahoo.com";
+    static const char YAHOO_QUERYSTR[] = "GET //search?p=up+open+university HTTP/1.0\n\n";
     
-	struct hostent *hostPtr=NULL;
-	struct sockaddr_in serverName = {0};
-	char *remoteHost=NULL;
-    int clientSocket, bytes, sent,total, status=0;
-    char message[1024],response[4096];
+    /* 
+     * Process id
+     */
+    pid_t pid;
     
-    int remotePort =        80;
-    char *host =        "api.twitter.com";
-    char *message_fmt = "GET /1.1/search/tweets.json?q=freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4 HTTP/1.0\n\n";
+    /*
+     * Time Related
+     */
+    time_t start_tdownload;
+    time_t end_tdownload;
+    int pid_holder;
+    double diff_tdownload;
+    double diff_tdownload2;
     
-    sprintf(message,message_fmt,"","");
-    printf("Request:\n%s\n",message);
-
-	clientSocket=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(-1 == clientSocket){
-		perror("socket");
-		exit(1);
-	}
-
-	/*
-	 * need to resolve the remote server name or IP address
-	 */
-
-	hostPtr = gethostbyname(host);
-	if(NULL == hostPtr){
-		hostPtr = gethostbyaddr(remoteHost,strlen(remoteHost),AF_INET);
-		if(NULL == hostPtr){
-			perror("Error resolving server address ");
-			exit(1);
-		}		
-	}
-
-	serverName.sin_family=AF_INET;
-	serverName.sin_port=htons(remotePort);
-	(void)memcpy(&serverName.sin_addr, hostPtr->h_addr, hostPtr->h_length);
-	status = connect(clientSocket, (struct sockaddr*)&serverName, sizeof(serverName));
+    /* 
+     * Hosts and sockaddr info
+     */
+    struct hostent *hostPtr=NULL;
+    struct sockaddr_in serverName = {0};
 	
-	if(-1 == status){
-		perror("connect");
-		exit(1);
+    /*
+     * Integer Declarations
+     */
+    int clientSocket;
+    int bytes;
+    int sent;
+    int received;
+        int received_bytes;
+        int received_bytes2;
+    int total;
+    int status=0;
+    int remotePort = 80;
+    
+    /*
+     * Char pointers and Strings
+     */
+    char message[MSG_STR_SIZE];
+    char response[MSG_STR_SIZE];
+    char *remoteHost=NULL;
+    char *host;
+    char *query_str;
+    
+    /*
+     * Make two copies of addresses
+     */
+    pid = fork();
+    printf("process id: %d\n", pid);
+    switch (pid) {
+        case -1:
+            perror("fork() process error");
+        case 0:
+            /*
+             * Set parent host and message fmt to google
+             */
+            host =        "google.com";
+            query_str = "GET /search?q=up+open+university HTTP/1.0\n\n";
+            break;
+        default:
+            host =        "search.yahoo.com";
+            query_str = "GET //search?p=up+open+university HTTP/1.0\n\n";
+            break;
+    }
+    /*
+     * Set message query string for HTTP GET
+     */
+    sprintf(message,query_str,"","");
+    /*
+     * Uncomment to see host server and GET query
+     * printf("URL: %s", host);
+     * printf("\nRequest:\n%s\n",message);
+     */
+    
+    /*
+     * Allocare socket descriptor and assign to clientSocket
+     */
+    clientSocket=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(-1 == clientSocket){
+        perror("socket");
+        exit(1);
+    }
+    
+    /*
+     * need to resolve the remote server name or IP address
+     */
+    hostPtr = gethostbyname(host);
+    if(NULL == hostPtr){
+        hostPtr = gethostbyaddr(remoteHost,strlen(remoteHost),AF_INET);
+        if(NULL == hostPtr){
+            perror("Error resolving server address ");
+            exit(1);
+        }
+    }
+    
+    /*
+     * Set socket struct addrinfo
+     */
+    serverName.sin_family=AF_INET;
+    serverName.sin_port=htons(remotePort);
+    (void)memcpy(&serverName.sin_addr, hostPtr->h_addr, hostPtr->h_length);
+    status = connect(clientSocket, (struct sockaddr*)&serverName, sizeof(serverName));
+    
+    /*
+     * Error checking
+     */
+    if(-1 == status){
+        perror("connect");
+        exit(1);
     }else{
-        printf("Connected to server\n");
+        printf(".... Calculating internet response time: \n");
     }
     total = strlen(message);
     sent = 0;
+    
+    /*
+     * Sending GET request to host
+     */
     do {
         bytes = write(clientSocket,message+sent,total-sent);
         if (bytes < 0)
@@ -93,17 +151,47 @@ int main(int argc, char *argv[]){
             break;
         sent+=bytes;
     } while (sent < total);
-    time(&start_t);
-
+    time(&start_tdownload);
     memset(response,0,sizeof(response));
-    int count = 0;
-    while(0 < (status =  read(clientSocket, response, sizeof(response)-1))){
-        printf("\n***** %d %s count[%d] \n", status, response, count);
-        count++;
+    
+    /*
+     * Received response from host
+     */
+    time(&start_tdownload);
+    total = sizeof(response)-1;
+    received = 0;
+    do {
+        bytes = read(clientSocket,response-received,total-received);
+        if (bytes < 0)
+            perror("ERROR reading response from socket");
+        if (bytes == 0)
+            break;
+        /*
+         * Add total bytes received
+         */
+        received+=bytes;
+    } while (bytes < 0);
+    /*
+     * Uncomment to see total bytes received and host response
+     *
+     * printf("\nResponse:\n%s\n",response);
+     * printf("\nReceived: %d\n", received);
+     */
+    time(&end_tdownload);
+    if (pid == 0) {
+        received_bytes = received;
+        diff_tdownload = difftime(end_tdownload, start_tdownload);
+    }else{
+        received_bytes2 = received;
+        diff_tdownload2 = difftime(end_tdownload, start_tdownload);
+        kill(pid, SIGKILL);
     }
+    /*
+     * Close client socket
+     */
     close(clientSocket);
-    time(&end_t);
-    diff_t = difftime(end_t, start_t);
-    printf("\n Execution time = %f\n", diff_t);
+    if(pid!=0){
+        printf("\nInternet speed connection is: Mbps%f\n", (((received_bytes+received_bytes2)/2)/((diff_tdownload+diff_tdownload2)/2))/1000);
+    }
 	return 0;
 }
